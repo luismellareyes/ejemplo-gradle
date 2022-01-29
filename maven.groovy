9c277ba7c1509d85b886025a37458c9e8f490219
luismella <luismellareyes@gmail.com>
2022-01-29 15:34:27 -0300

    modificacion gradle

--- maven.groovy
+++ maven.groovy
@@ -21,7 +21,7 @@ def call(){
         }
     }
     stage("Paso 5: Curl Springboot Gradle sleep 60"){
-        sh "mvn spring-boot:run"
+        sh "mvn spring-boot:run &"
         sh "sleep 60 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
     stage("Paso 6: Subir Nexus"){
